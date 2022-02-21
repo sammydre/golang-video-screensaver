3c6f23b87d825b12fa2a13b630b8853fd78be089
Sam Jansen <sam.jansen@gmail.com>
2022-02-21 20:54:34 +0000

    Fixes for GitHub actions.
    Hopefully the build script will work in newer PowerShell.
    
    Hopefully enable tests.

--- build.ps1
+++ build.ps1
@@ -18,29 +18,27 @@ function Invoke-NativeCommand {
 # This function name doesn't quite follow the "<ApprovedVerb>-<Prefix><SingularNoun>"
 # naming convention... Oh well!
 function Get-And-Extract-Zip {
-    param([String]$Destination="") 
+    param(
+        [String]$Destination="",
+        [String]$Uri
+    )
 
-    $url = $args[0]
-
-    Write-Output "Downloading $url"
+    Write-Output "Downloading $Uri"
 
     $tmp = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } -PassThru
-    Invoke-WebRequest $url -OutFile $tmp
+    Invoke-WebRequest -Uri $Uri -OutFile $tmp
     Expand-Archive -Path $tmp -DestinationPath "$PSScriptRoot\out\$Destination"
     Remove-Item -Path $tmp
 }
 
-# TODO:
-#  - actually write the installer using go:ember
-
 $ignored=New-Item -Path .\out -ItemType Directory -Force
 
 if (-not(Test-Path -Path "out\upx-3.96-win64\upx.exe" -PathType Leaf)) {
-    Get-And-Extract-Zip "https://github.com/upx/upx/releases/download/v3.96/upx-3.96-win64.zip"
+    Get-And-Extract-Zip -Uri "https://github.com/upx/upx/releases/download/v3.96/upx-3.96-win64.zip"
 }
 
 if (-not(Test-Path -Path "out\libvlc-3.0.16\build\x64\libvlc.dll" -PathType Leaf)) {
-    Get-And-Extract-Zip "https://www.nuget.org/api/v2/package/VideoLAN.LibVLC.Windows/3.0.16" -Destination "libvlc-3.0.16"
+    Get-And-Extract-Zip -Uri "https://www.nuget.org/api/v2/package/VideoLAN.LibVLC.Windows/3.0.16" -Destination "libvlc-3.0.16"
 }
 
 if ($DownloadOnly.IsPresent) {
